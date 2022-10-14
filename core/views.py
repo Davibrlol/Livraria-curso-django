148212ff568d2b6f32e51b5d95c1093bd8879630
davi <daviescolaifc@gmail.com>
2022-10-14 15:07:53 -0300

    Feito vídeo 17

--- core/views.py
+++ core/views.py
@@ -1,7 +1,23 @@
+import imp
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator 
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from core.models import Categoria
+
+import json
 
 def teste(request):
     return HttpResponse('Teste')
 
 def teste2(request):
+    
     return HttpResponse('Teste2') 
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoriaView(View):
+    def get(self, request ):
+        data = list(Categoria.objects.values())
+        formatted_data = json.dumps(data, ensure_ascii=False)
+        return HttpResponse(formatted_data, content_type="application/json")
