4cfa2962e17dae1b6232437db129cb0c2d5ada57
davi <daviescolaifc@gmail.com>
2022-10-26 10:48:04 -0300

    Feito vídeo 21

--- livraria/urls.py
+++ livraria/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste/', views.teste),
     path('teste2/', views.teste2),
-    path('categorias/', views.CategoriaView.as_view())
-]
+    path('categorias/', views.CategoriaView.as_view()),
+    path('categorias/<int:id>/', views.CategoriaView.as_view()),
+    path('categorias-apiview/', views.CategoriasList.as_view()),
+    path('categorias-apiview/<int:id>/', views.CategoriaDetail.as_view()),
+    ]
